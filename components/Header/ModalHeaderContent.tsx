92f216d6359606aa3f90b88c412072b1964163ec
Fredy Daniel Flores Lemus <56806728+fredydlemus@users.noreply.github.com>
2022-10-16 13:05:33 -0600

    Complete Header component

--- components/Header/ModalHeaderContent.tsx
+++ components/Header/ModalHeaderContent.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Header } from 'semantic-ui-react';
+import { Die } from '@components/SVGIcons';
 
 const ModalHeaderContent = () => {
     return (
-        <div>ModalHeaderContent</div>
+        <div className='container'>
+            <Grid columns={2}>
+                <Grid.Row verticalAlign='middle'>
+                    <Grid.Column width='5'>
+                        <Die size='154px' />
+                    </Grid.Column>
+                    <Grid.Column width='11'>
+                        <Header as='h2'>You killed the avocado</Header>
+                        <p>You touched it so much you killed it.</p>
+                        <p>On the other hand, you are a very curious person and you have discovered
+                            this. :)</p>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            <style jsx>{`
+                .container {
+                    padding: 2rem;
+                }
+            `}</style>
+        </div>
     )
 }
 
--- components/Header/RottenHeader.tsx
+++ components/Header/RottenHeader.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Header } from 'semantic-ui-react';
+import { Die } from '@components/SVGIcons';
 
 const RottenHeader = () => {
     return (
-        <div>RottenHeader</div>
+        <Header size='huge' as='h1'>
+            Avo
+            <Die size='58px' />
+            Shop
+        </Header>
     )
 }
 
